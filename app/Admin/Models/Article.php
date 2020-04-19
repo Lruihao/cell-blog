9af056f10113053a4a895f6b491528758b537366
李瑞豪 <1074627678@qq.com>
2020-04-19 23:11:01 +0800

    新增： markdown转HTML

--- app/Admin/Models/Article.php
+++ app/Admin/Models/Article.php
@@ -3,8 +3,15 @@
 namespace App\Admin\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class Article extends Model
 {
-    //
+    public static function boot()
+    {
+        parent::boot();
+        static::saving(function ($article) {
+            $article->html = Markdown::convertToHtml($article->markdown);
+        });
+    }
 }
