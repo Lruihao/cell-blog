0d4b813a2954b9d6f9280c337e5f333cd09463ff
李瑞豪 <1074627678@qq.com>
2020-04-19 14:17:42 +0800

    更新： 文章表结构

--- database/migrations/2020_04_17_120903_create_articles_table.php
+++ database/migrations/2020_04_17_120903_create_articles_table.php
@@ -16,14 +16,14 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function(Blueprint $table) {
             $table->increments('id')->comment('文章表主键');
             $table->string('title')->default('')->comment('标题');
-            $table->string('author')->default('')->comment('作者'); 
-            $table->boolean('category_id')->default(0)->comment('分类id');
+            $table->integer('user_id')->unsigned()->default(0)->comment('作者ID');
+            $table->integer('category_id')->unsigned()->default(0)->comment('分类ID');
             $table->string('description')->default('')->comment('描述');
             $table->string('keywords')->default('')->comment('关键词');
-            $table->integer('sort')->default(0)->comment('置顶排序');
-            $table->string('password')->default('')->comment('文章密码');
+            $table->integer('sort')->unsigned()->default(0)->comment('置顶排序');
+            $table->string('password')->nullable()->default('')->comment('文章密码');
             $table->integer('views')->unsigned()->default(0)->comment('阅读数量');
-            $table->longText('markdown')->nullable()->comment('markdown文章内容');
+            $table->longText('markdown')->comment('markdown文章内容');
             $table->longText('html')->comment('markdown转的html页面');
             $table->timestamps();
         });
