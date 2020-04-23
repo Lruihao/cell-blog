c46311d9931d52717438fd889d8d84b7eef6c1d2
李瑞豪 <1074627678@qq.com>
2020-04-23 20:42:27 +0800

    新增：分类一对多关联文章

--- app/Admin/Models/Category.php
+++ app/Admin/Models/Category.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    /**
+     * 关联文章表
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
