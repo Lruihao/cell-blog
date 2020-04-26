f2a7cd083d5b08d9bf286086a8666e8fdc045828
李瑞豪 <1074627678@qq.com>
2020-04-26 21:16:19 +0800

    新增：后台锁定功能

--- config/admin.php
+++ config/admin.php
@@ -62,7 +62,7 @@ return [
 
         'namespace' => 'App\\Admin\\Controllers',
 
-        'middleware' => ['web', 'admin'],
+        'middleware' => ['web', 'admin', 'admin.lock'],
     ],
 
     /*
