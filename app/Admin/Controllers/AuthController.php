b7e3ad5a805023436a3a821a776e6a5e2588510b
李瑞豪 <1074627678@qq.com>
2020-04-26 11:18:20 +0800

    新增：登录验证码

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -3,8 +3,60 @@
 namespace App\Admin\Controllers;
 
 use Encore\Admin\Controllers\AuthController as BaseAuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Lang;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends BaseAuthController
 {
+    /**
+     * 获取登录视图
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|Redirect|\Illuminate\View\View
+     */
+    public function getLogin()
+    {
+        if (!Auth::guard('admin')->guest()) {
+            return redirect(config('admin.route.prefix'));
+        }
+        return view('admin.login');
+    }
+
+    /**
+     * 登录验证
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|mixed
+     */
+    public function postLogin(Request $request)
+    {
+        $credentials = $request->only(['username', 'password', 'captcha']);
+        $validator = Validator::make($credentials, [
+            'username' => 'required',
+            'password' => 'required',
+            'captcha' => 'required|captcha'
+        ]);
+        if ($validator->fails()) {
+            return Redirect::back()->withInput()->withErrors($validator);
+        }
+        unset($credentials['captcha']);
+        if (Auth::guard('admin')->attempt($credentials)) {
+            admin_toastr(trans('admin.login_successful'));
+            return redirect()->intended(config('admin.route.prefix'));
+        }
+        return Redirect::back()->withInput()
+            ->withErrors(['username' => $this->getFailedLoginMessage()]);
+    }
+
+    /**
+     * 获取登录错误信息
+     * @return array|\Illuminate\Contracts\Translation\Translator|string|\Symfony\Component\Translation\TranslatorInterface|null
+     */
+    protected function getFailedLoginMessage()
+    {
+        return Lang::has('auth.failed')
+            ? trans('auth.failed')
+            : 'These credentials do not match our records.';
+    }
 
 }
--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Mews\Captcha\CaptchaServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
 
     ],
 
