b383dadc3ab1f878a86ff9935dd80f7bc16ec05d
李瑞豪 <1074627678@qq.com>
2020-05-04 15:53:30 +0800

    修改：Cell Blog系统默认配置

--- config/blog.php
+++ config/blog.php
@@ -8,11 +8,11 @@ return [
     | ！！！请勿修改该配置文件，自定义请前往 http://your.host/admin/config
     |
     */
-    'title' => 'Cell-Blog',
-    'blog_name' => 'Cell-Blog',
-    'description' => '基于laravel开发的Markdown博客，李瑞豪的个人独立博客系统',
-    'keywords' => 'Cell-Blog,李瑞豪,lruihao,独立博客,laravel-blog',
-    'welcome_words' => '欢迎使用 Cell-Blog 个人独立博客！',
+    'title' => 'Cell Blog',
+    'blog_name' => 'Cell Blog',
+    'description' => '基于PHP框架Laravel7开发的Markdown个人独立博客,Powered by Cell Blog,Author: Github@Lruihao',
+    'keywords' => 'Cell-Blog,Laravel-Blog,Markdown,PHP,个人独立博客,Lruihao,李瑞豪',
+    'welcome_words' => '欢迎使用 Cell Blog 个人独立博客！',
     'github_url' => 'https://github.com/Lruihao',
     'weibo_url' => 'https://weibo.com/liahao',
     /*
