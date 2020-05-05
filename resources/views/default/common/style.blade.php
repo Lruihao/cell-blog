99d0000f8097eb1fb83f883542433375441e180e
李瑞豪 <1074627678@qq.com>
2020-05-05 20:35:10 +0800

    新增：沐目体压缩版

--- resources/views/default/common/style.blade.php
+++ resources/views/default/common/style.blade.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="{{ asset('/libs/share.js/css/share.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/libs/share.js/css/share.min.css') }}"/>
 <link rel="stylesheet" href="{{ asset('/libs/emojify.js/css/emojify.min.css') }}"/>
-<link rel="stylesheet" href="{{ asset('/libs/highlight.js/css/tomorrow-night-eighties.css') }}">
+<link rel="stylesheet" href="{{ asset('/libs/highlight.js/css/tomorrow-night-eighties.css') }}"/>
 <link rel="stylesheet" href="{{ asset('/css/article.css') }}"/>
