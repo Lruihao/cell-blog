a1bf90059c4a7a3f6391705d78ae7aeaab9b952e
李瑞豪 <1074627678@qq.com>
2020-05-05 18:50:02 +0800

    新增：搜索结果数目显示

--- resources/views/default/search_article.blade.php
+++ resources/views/default/search_article.blade.php
@@ -12,6 +12,7 @@
             <h1 class="display-5 mb-3">
                 <i class="fas fa-search header-icon" title="搜索: {{ request('wd') }}" data-toggle="tooltip" data-placement="left"></i>
                 {{ request('wd') }}
+                <span class="header-badge badge badge-pill badge-secondary">{{ count($articles) }}</span>
             </h1>
         </div>
     </div>
