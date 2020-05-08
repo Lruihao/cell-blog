9cc3feb38950bb3caa0bb8e42533f731ec437c7f
李瑞豪 <1074627678@qq.com>
2020-05-08 16:49:27 +0800

    修改：格式化数据种子

--- database/seeds/MenuSeeder.php
+++ database/seeds/MenuSeeder.php
@@ -14,68 +14,69 @@ class MenuSeeder extends Seeder
     {
         DB::table('admin_menu')->insert([
             [
-            'id' => 9,
-            'parent_id' => 0,
-            'order' => 9,
-            'title' => 'Article',
-            'icon' => 'fa-edit',
-            'uri' => ''
-        ],[
-            'id' => 10,
-            'parent_id' => 9,
-            'order' => 10,
-            'title' => 'Articles',
-            'icon' => 'fa-edit',
-            'uri' => '/articles'
-        ],[
-            'id' => 11,
-            'parent_id' => 9,
-            'order' => 11,
-            'title' => 'Categories',
-            'icon' => 'fa-th',
-            'uri' => '/categories'
-        ],[
-            'id' => 12,
-            'parent_id' => 9,
-            'order' => 12,
-            'title' => 'Tags',
-            'icon' => 'fa-tags',
-            'uri' => '/tags'
-        ],[
-            'id' => 13,
-            'parent_id' => 0,
-            'order' => 13,
-            'title' => 'Pages',
-            'icon' => 'fa-pagelines',
-            'uri' => '/pages'
-        ],[
-            'id' => 14,
-            'parent_id' => 0,
-            'order' => 14,
-            'title' => 'Navigations',
-            'icon' => 'fa-navicon',
-            'uri' => '/navigations'
-        ],[
-            'id' => 15,
-            'parent_id' => 0,
-            'order' => 15,
-            'title' => 'Mottoes',
-            'icon' => 'fa-book',
-            'uri' => '/mottoes'
-        ],[
-            'id' => 16,
-            'parent_id' => 0,
-            'order' => 16,
-            'title' => 'Links',
-            'icon' => 'fa-user-plus',
-            'uri' => '/links'
-        ],[
-            'id' => 17,
-            'parent_id' => 0,
-            'order' => 17,
-            'title' => 'Systems',
-            'icon' => 'fa-toggle-on',
-            'uri' => '/systems'
-        ]]);
+                'id' => 9,
+                'parent_id' => 0,
+                'order' => 9,
+                'title' => 'Article',
+                'icon' => 'fa-edit',
+                'uri' => ''
+            ], [
+                'id' => 10,
+                'parent_id' => 9,
+                'order' => 10,
+                'title' => 'Articles',
+                'icon' => 'fa-edit',
+                'uri' => '/articles'
+            ], [
+                'id' => 11,
+                'parent_id' => 9,
+                'order' => 11,
+                'title' => 'Categories',
+                'icon' => 'fa-th',
+                'uri' => '/categories'
+            ], [
+                'id' => 12,
+                'parent_id' => 9,
+                'order' => 12,
+                'title' => 'Tags',
+                'icon' => 'fa-tags',
+                'uri' => '/tags'
+            ], [
+                'id' => 13,
+                'parent_id' => 0,
+                'order' => 13,
+                'title' => 'Pages',
+                'icon' => 'fa-pagelines',
+                'uri' => '/pages'
+            ], [
+                'id' => 14,
+                'parent_id' => 0,
+                'order' => 14,
+                'title' => 'Navigations',
+                'icon' => 'fa-navicon',
+                'uri' => '/navigations'
+            ], [
+                'id' => 15,
+                'parent_id' => 0,
+                'order' => 15,
+                'title' => 'Mottoes',
+                'icon' => 'fa-book',
+                'uri' => '/mottoes'
+            ], [
+                'id' => 16,
+                'parent_id' => 0,
+                'order' => 16,
+                'title' => 'Links',
+                'icon' => 'fa-user-plus',
+                'uri' => '/links'
+            ], [
+                'id' => 17,
+                'parent_id' => 0,
+                'order' => 17,
+                'title' => 'Systems',
+                'icon' => 'fa-toggle-on',
+                'uri' => '/systems'
+            ]
+        ]);
     }
 }
--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -12,48 +12,50 @@ class PermissionSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('admin_permissions')->insert([[
-            'id' => 7,
-            'name' => 'article',
-            'slug' => 'article',
-            'http_method' => '',
-            'http_path' => '/articles*'
-        ],[
-            'id' => 8,
-            'name' => 'category',
-            'slug' => 'category',
-            'http_method' => '',
-            'http_path' => '/categories*'
-        ],[
-            'id' => 9,
-            'name' => 'tag',
-            'slug' => 'tag',
-            'http_method' => '',
-            'http_path' => '/tags*'
-        ],[
-            'id' => 10,
-            'name' => 'page',
-            'slug' => 'page',
-            'http_method' => '',
-            'http_path' => '/pages*'
-        ],[
-            'id' => 11,
-            'name' => 'motto',
-            'slug' => 'motto',
-            'http_method' => '',
-            'http_path' => '/mottoes*'
-        ],[
-            'id' => 12,
-            'name' => 'friendship-link',
-            'slug' => 'friendship link',
-            'http_method' => '',
-            'http_path' => '/links*'
-        ],[
-            'id' => 13,
-            'name' => 'system',
-            'slug' => 'system config',
-            'http_method' => '',
-            'http_path' => '/systems*'
-        ]]);
+        DB::table('admin_permissions')->insert([
+            [
+                'id' => 7,
+                'name' => 'article',
+                'slug' => 'article',
+                'http_method' => '',
+                'http_path' => '/articles*'
+            ], [
+                'id' => 8,
+                'name' => 'category',
+                'slug' => 'category',
+                'http_method' => '',
+                'http_path' => '/categories*'
+            ], [
+                'id' => 9,
+                'name' => 'tag',
+                'slug' => 'tag',
+                'http_method' => '',
+                'http_path' => '/tags*'
+            ], [
+                'id' => 10,
+                'name' => 'page',
+                'slug' => 'page',
+                'http_method' => '',
+                'http_path' => '/pages*'
+            ], [
+                'id' => 11,
+                'name' => 'motto',
+                'slug' => 'motto',
+                'http_method' => '',
+                'http_path' => '/mottoes*'
+            ], [
+                'id' => 12,
+                'name' => 'friendship-link',
+                'slug' => 'friendship link',
+                'http_method' => '',
+                'http_path' => '/links*'
+            ], [
+                'id' => 13,
+                'name' => 'system',
+                'slug' => 'system config',
+                'http_method' => '',
+                'http_path' => '/systems*'
+            ]
+        ]);
     }
 }
